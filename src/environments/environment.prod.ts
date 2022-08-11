0c9e32d17fcab59f25220e50149fb5ae91bbc09a
gustavogalveza <gustavogalveza@gmail.com>
2022-08-10 22:09:22 -0400

    [Frontend] CHG environment apiKey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://api.cmfchile.cl/api-sbifv3/recursos_api/'
+  api: 'https://api.cmfchile.cl/api-sbifv3/recursos_api/',
+  apiKey: '9ef4dda37efb2a85e3e80f5411bcc6ff5f84fe6c',
 };
